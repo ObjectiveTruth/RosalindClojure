a1dda90d13ca4d7843e00b47e538c746357a0d15
ObjectiveTruth-Laptop <josemiguel.mendez@uoit.net>
2015-02-05 00:55:23 -0500

    Updated the project.clj with the dependancies, description, and license terms

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject rosalindclojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :description "Rosalind Problems solved in Clojure"
+  :url "http://github.com/ObjectiveTruth/RosalindClojure"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+  				[aysylu/loom "0.5.0"]
+  				]
   :main ^:skip-aot rosalindclojure.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
